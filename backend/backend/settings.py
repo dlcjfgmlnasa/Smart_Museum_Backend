b5d85251405d6fe6ecb6e486a9b443533bffa1ec
이철희 <dlcjfgmlnasa@naver.com>
2022-07-05 03:39:25 +0900

    setting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -147,11 +147,12 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
 }
 
-CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000',
-                         'http://localhost:3000',
-                         'http://59.19.102.174:8888']
-
-CORS_ALLOW_CREDENTIALS = True
+# CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000',
+#                          'http://localhost:3000',
+#                          'http://59.19.102.174:8888']
+#
+# CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
