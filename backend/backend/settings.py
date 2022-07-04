fab439d563fcf998a75262c1e78370c748428800
이철희 <dlcjfgmlnasa@naver.com>
2022-07-05 00:14:46 +0900

    media 경로

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -162,13 +162,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'config', 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'config', 'static')
+# ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, '_files', 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, '_files', 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
