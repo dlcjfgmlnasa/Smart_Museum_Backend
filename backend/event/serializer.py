eea0968717ea7ce7158022f3875a2b412336fa5c
이철희 <dlcjfgmlnasa@naver.com>
2022-07-06 05:21:05 +0900

    EVENT GET - inner exhibition 추가

--- backend/event/serializer.py
+++ backend/event/serializer.py
@@ -1,11 +1,14 @@
 # -*- coding:utf-8 -*-
 from .models import Event
+from museum.serializer import InnerExhibitionSerializer
 from account.serializer import AccountSerializer
 from museum.serializer import ExhibitionSerializer
 from rest_framework.serializers import ModelSerializer
 
 
 class EventSimpleSerializer(ModelSerializer):
+    inner_exhibition = InnerExhibitionSerializer(many=True)
+
     class Meta:
         model = Event
         fields = (
@@ -15,7 +18,8 @@ class EventSimpleSerializer(ModelSerializer):
             'end_dt',
             'type',
             'explanation',
-            'image'
+            'image',
+            'inner_exhibition'
         )
 
 
