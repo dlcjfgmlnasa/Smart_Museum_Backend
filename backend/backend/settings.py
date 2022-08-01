2259e9b4b56d8b40d00bd9e3b54eb54f66ff4efd
이철희 <dlcjfgmlnasa@naver.com>
2022-08-02 00:23:17 +0900

    CORS => locslab.com 추가

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -156,6 +156,7 @@ CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000',
                          'http://localhost:5000',
                          'http://127.0.0.1:5000',
                          'http://59.19.102.174:8888',
+                         'http://locslab.com',
                          'http://locslab.com:8888',
                          'https://locslab.com:8888']
 
