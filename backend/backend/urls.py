6658385bffb200266c2fa91a95c925fce04a2b42
이철희 <dlcjfgmlnasa@naver.com>
2022-10-24 05:58:41 +0900

    Swagger API 추가

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -94,10 +94,8 @@ urlpatterns += [
     *static(settings.MEDIA_URL_1, document_root=settings.MEDIA_ROOT_1)
 ]
 
-
-if settings.DEBUG:
-    urlpatterns += [
-        re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-        re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-        re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    ]
+urlpatterns += [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
